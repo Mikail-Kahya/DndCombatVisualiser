115aa19fb3906e5eae456fee31565578861b43ca
Mikail Kahya <138559177+Mikail-Kahya@users.noreply.github.com>
2025-08-24 17:25:41 +0200

    feat: a grid made out of shapes component

--- CombatVisualiser/Components/Map/ShapeGridComponent.cpp
+++ CombatVisualiser/Components/Map/ShapeGridComponent.cpp
@@ -1,3 +1,42 @@
 #include "ShapeGridComponent.h"
 
+#include "GameObject.h"
+#include "SceneManager.h"
+#include "ShapeComponent.h"
+
 using namespace mk;
+
+ShapeGridComponent::ShapeGridComponent(int nrRows, int nrCols, float gap)
+	: m_Rows{ std::max(nrRows, 1) }
+	, m_Cols{ std::max(nrCols, 1) }
+	, m_Gap{ gap }
+{
+}
+
+void ShapeGridComponent::Start()
+{
+	IComponent::Start();
+
+	auto& scene{ SceneManager::GetInstance().GetScene() };
+
+	auto* templateShapePtr = GetOwner()->AddComponent<ShapeComponent>();
+	templateShapePtr->SetVisibility(false);
+
+	for (int row{}; row < m_Rows; ++row)
+	{
+		const float y{ row * (templateShapePtr->GetHeight() + m_Gap) };
+		for (int col{}; col < m_Cols; ++col)
+		{
+			const float x{ col * (templateShapePtr->GetWidth() + m_Gap) };
+			auto cellPtr{ scene.SpawnObject("Shape") };
+			cellPtr->SetLocalPosition({ x, y });
+			cellPtr->SetParent(GetOwner());
+
+			auto cellShapePtr{ cellPtr->AddComponent<ShapeComponent>(templateShapePtr->GetPoints()) };
+			cellShapePtr->SetFill(templateShapePtr->IsFilled());
+			cellShapePtr->SetColor(templateShapePtr->GetColor());
+			cellShapePtr->SetCloseShape(templateShapePtr->IsClosed());
+			cellShapePtr->SetDepth(templateShapePtr->GetDepth());
+		}
+	}
+}
--- CombatVisualiser/Components/Map/ShapeGridComponent.h
+++ CombatVisualiser/Components/Map/ShapeGridComponent.h
@@ -4,17 +4,32 @@
 
 #include "IComponent.h"
 
+/*
+ * Creates child objects with the shape component information
+ * Rows and columns are [1,+inf[
+*/
+// If optimizing is needed, use one base vector for the vertices and apply this to all shapes
+
 namespace mk
 {
+	class ShapeComponent;
+
 	class ShapeGridComponent : public IComponent
 	{
-	public:
-		ShapeGridComponent() = default;
+	public:	
+		ShapeGridComponent(int nrRows, int nrCols, float gap = 0.f);
 		~ShapeGridComponent() override = default;
 
 		ShapeGridComponent(const ShapeGridComponent& other)					= delete;
 		ShapeGridComponent(ShapeGridComponent&& other) noexcept				= delete;
 		ShapeGridComponent& operator=(const ShapeGridComponent& other)		= delete;
 		ShapeGridComponent& operator=(ShapeGridComponent&& other) noexcept	= delete;
+
+		void Start() override;
+
+	private:
+		int m_Rows{ 1 };
+		int m_Cols{ 1 };
+		float m_Gap{};
 	};
 }

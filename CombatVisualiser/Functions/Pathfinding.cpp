feb6a04667bda44a3c2585eaa3f30696d68a9171
Mikail Kahya <138559177+Mikail-Kahya@users.noreply.github.com>
2025-08-27 21:49:31 +0200

    feat: Astar calculator

--- CombatVisualiser/Functions/Pathfinding.cpp
+++ CombatVisualiser/Functions/Pathfinding.cpp
@@ -1,7 +1,13 @@
 #include "Pathfinding.h"
 
+#include <algorithm>
+
 using namespace mk;
 using namespace algo;
+
+/* ===============================
+ * GRAPH
+ * ===============================*/
 
 Graph::Graph(const Graph& other)
 	: connections{ other.connections }
@@ -21,7 +27,7 @@ Graph& Graph::operator=(const Graph& other)
 
 Connection* Graph::AddConnection(size_t fromIdx, size_t toIdx, float weight) noexcept
 {
-	if (nodes.size() < fromIdx || nodes.size() < toIdx)
+	if (nodes.size() <= fromIdx || nodes.size() <= toIdx)
 		return nullptr;
 
 	connections.emplace_back(Connection{ nodes[fromIdx].get(), nodes[toIdx].get(), weight });
@@ -30,7 +36,7 @@ Connection* Graph::AddConnection(size_t fromIdx, size_t toIdx, float weight) noe
 
 bool Graph::RemoveConnection(size_t fromIdx, size_t toIdx) noexcept
 {
-	if (nodes.size() < fromIdx || nodes.size() < toIdx)
+	if (nodes.size() <= fromIdx || nodes.size() <= toIdx)
 		return false;
 
 	const Node* fromNodePtr{ nodes[fromIdx].get() };
@@ -45,17 +51,146 @@ bool Graph::RemoveConnection(size_t fromIdx, size_t toIdx) noexcept
 	return nrConnections;
 }
 
+std::vector<Connection> Graph::GetConnectionsFromNode(size_t idx) const noexcept
+{
+	if (nodes.size() <= idx)
+		return {};
+	return GetConnectionsFromNode(nodes[idx].get());
+}
+
+std::vector<Connection> Graph::GetConnectionsFromNode(Node* nodePtr) const noexcept
+{
+	std::vector<Connection> nodeConnections{};
+	for (const Connection& connection : connections)
+	{
+		if (connection.fromNodePtr == nodePtr) 
+			nodeConnections.emplace_back(connection);
+	}
+	return nodeConnections;
+}
+
+/* ===============================
+ * ASTAR
+ * ===============================*/
+
 AStar::AStar(Graph&& graph, const Heuristic& heuristicFunc)
 	: m_Graph{ std::move(graph) }
 	, m_HeuristicFunc{ heuristicFunc }
 {
 }
 
-void AStar::FindPath()
+bool AStar::FindPath(size_t startIdx, size_t goalIdx, size_t maxSteps)
 {
+	if (m_Graph.nodes.size() < startIdx || m_Graph.nodes.size() < goalIdx)
+		return false;
+	return FindPath(m_Graph.nodes[startIdx].get(), m_Graph.nodes[goalIdx].get(), maxSteps);
 }
 
-const std::vector<Connection>& AStar::GetPath() const
+bool AStar::FindPath(Node* startNodePtr, Node* goalNodePtr, size_t maxSteps)
+{
+	if (!startNodePtr || !goalNodePtr)
+		return false;
+
+	bool foundPath{ false };
+
+	std::list<NodeRecord> openList{};
+	std::list<NodeRecord> closedList{};
+
+	auto replaceNode = [](std::list<NodeRecord>& list, const NodeRecord& newRecord)
+		{
+			for (NodeRecord& record : list)
+			{
+				if (record.nodePtr == newRecord.nodePtr && record.costSoFar >= newRecord.costSoFar)
+				{
+					record = newRecord;
+					return true;
+				}
+			}
+			return false;
+			//std::replace_if(list.begin(), list.end(), [&newRecord](const NodeRecord& record)
+			//	{
+			//		return record.nodePtr == newRecord.nodePtr && record.costSoFar >= newRecord.costSoFar;
+			//	}, newRecord);
+		};
+
+	size_t currentStep{};
+
+	NodeRecord currentRecord{};
+	currentRecord.nodePtr = startNodePtr;
+	currentRecord.estimatedTotalCost = GetHeuristicCost(startNodePtr, goalNodePtr);
+
+	openList.push_back(currentRecord);
+
+	// Water => Use estimated cost
+
+	while (!openList.empty())
+	{
+		if (currentStep > maxSteps)
+			break;
+
+		++currentStep;
+
+		// Get connection with the lowest F score (estimatedTotalCost)
+		// std::min_element uses the < and == overloaded operator
+
+		currentRecord = *std::min_element(openList.begin(), openList.end());
+		if (currentRecord.nodePtr == goalNodePtr)
+		{
+			foundPath = true;
+			break;
+		}
+
+		const auto& connections{ m_Graph.GetConnectionsFromNode(currentRecord.nodePtr) };
+		for (const Connection& connection : connections)
+		{
+			NodeRecord newRecord{};
+			newRecord.nodePtr = connection.toNodePtr;
+			newRecord.connection = connection;
+			newRecord.costSoFar = currentRecord.costSoFar + connection.weight;
+			newRecord.estimatedTotalCost = newRecord.costSoFar + GetHeuristicCost(newRecord.nodePtr, goalNodePtr);
+
+			// Remove/override any nodes found in the list and with a cheaper cost
+			if (replaceNode(openList, newRecord))
+				continue;
+			if (replaceNode(closedList, newRecord))
+				continue;
+
+			openList.push_back(newRecord);
+		}
+
+		closedList.push_back(currentRecord);
+		openList.remove(currentRecord);
+	}
+
+	if (foundPath)
+	{
+		m_Path.clear();
+		while (currentRecord.nodePtr != startNodePtr)
+		{
+			m_Path.push_back(currentRecord.nodePtr);
+			Node* const currentStartNodePtr{ currentRecord.connection.fromNodePtr };
+
+			for (const NodeRecord& record : closedList)
+			{
+				if (record.nodePtr != currentStartNodePtr)
+					continue;
+
+				currentRecord = record;
+				break;
+			}
+		}
+
+		m_Path.push_back(startNodePtr);
+
+		// reverse path
+		std::ranges::reverse(m_Path);
+	}
+	
+
+	return foundPath;
+}
+
+const std::vector<Node*>& AStar::GetPath() const
 {
 	return m_Path;
 }
@@ -63,4 +198,10 @@ const std::vector<Connection>& AStar::GetPath() const
 Graph& AStar::GetGraph()
 {
 	return m_Graph;
+}
+
+float AStar::GetHeuristicCost(Node* fromNodePtr, Node* toNodePtr) const
+{
+	return m_HeuristicFunc(	abs(toNodePtr->GetX() - fromNodePtr->GetX()), 
+							abs(toNodePtr->GetY() - fromNodePtr->GetY()));
 }
--- CombatVisualiser/Functions/Pathfinding.h
+++ CombatVisualiser/Functions/Pathfinding.h
@@ -4,6 +4,8 @@
 #include <memory>
 #include <functional>
 
+#include "glm/vec2.hpp"
+
 namespace mk
 {
 	namespace algo
@@ -15,7 +17,11 @@ namespace mk
 		====================================== 
 		*/
 
-		struct Node // Struct to be overidden during a graph
+		// Struct to be overidden during a graph
+		// Nodes can also be in an imaginary space
+		//	E.g - Grids can work with coordinates
+		//	    - World space
+		struct Node 
 		{
 			Node()			= default;
 			virtual ~Node() = default;
@@ -26,13 +32,22 @@ namespace mk
 			Node& operator=(Node&& other) noexcept	= delete;
 
 			virtual std::unique_ptr<Node> Clone() const = 0;
-		}; 
+			virtual float GetX() const = 0;
+			virtual float GetY() const = 0;
+		};
 
 		struct Connection final
 		{
 			Node* fromNodePtr{};
 			Node* toNodePtr{};
 			float weight{};
+
+			bool operator==(const Connection& other) const
+			{
+				return fromNodePtr == other.fromNodePtr
+					&& toNodePtr == other.toNodePtr
+					&& abs(weight - other.weight) < FLT_EPSILON;
+			}
 		};
 
 		struct Graph final
@@ -46,6 +61,9 @@ namespace mk
 
 			Connection* AddConnection(size_t fromIdx, size_t toIdx, float weight) noexcept;
 			bool RemoveConnection(size_t fromIdx, size_t toIdx) noexcept;
+
+			std::vector<Connection> GetConnectionsFromNode(size_t idx) const noexcept;
+			std::vector<Connection> GetConnectionsFromNode(Node* nodePtr) const noexcept;
 
 			std::vector<Connection> connections{};
 			std::vector<std::unique_ptr<Node>> nodes{};
@@ -62,14 +80,40 @@ namespace mk
 			AStar& operator=(const AStar& other)		= delete;
 			AStar& operator=(AStar&& other) noexcept	= delete;
 
-			void FindPath();
-			const std::vector<Connection>& GetPath() const;
+			// Fills up the path
+			// Returns whether it finds a path possible
+			bool FindPath(size_t startIdx, size_t goalIdx, size_t maxSteps = 100);
+			bool FindPath(Node* startNodePtr, Node* goalNodePtr, size_t maxSteps = 100);
+			const std::vector<Node*>& GetPath() const;
 			Graph& GetGraph(); // Returns a reference to adjust the graph in the future
 
 		private:
+			struct NodeRecord final
+			{
+				Node* nodePtr{};
+				Connection connection{};
+				float costSoFar = 0.f; // accumulated g-costs of all the connections leading up to this one
+				float estimatedTotalCost = 0.f; // f-cost (= costSoFar + h-cost)
+
+				bool operator==(const NodeRecord& other) const
+				{
+					return nodePtr == other.nodePtr
+						&& connection == other.connection
+						&& abs(costSoFar - other.costSoFar) < FLT_EPSILON
+						&& abs(estimatedTotalCost - other.estimatedTotalCost) < FLT_EPSILON;
+				}
+
+				bool operator<(const NodeRecord& other) const
+				{
+					return estimatedTotalCost < other.estimatedTotalCost;
+				}
+			};
+
+			float GetHeuristicCost(Node* fromNodePtr, Node* toNodePtr) const;
+
 			Graph m_Graph{};
 			const Heuristic m_HeuristicFunc{};
-			std::vector<Connection> m_Path{};
+			std::vector<Node*> m_Path{};
 		};
 	}
 }

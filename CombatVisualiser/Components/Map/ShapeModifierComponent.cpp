1652bcae6c73f3f507c160327986e4698fd6de60
Mikail Kahya <138559177+Mikail-Kahya@users.noreply.github.com>
2025-08-31 16:00:12 +0200

    fix: remove observer

--- CombatVisualiser/Components/Map/ShapeModifierComponent.cpp
+++ CombatVisualiser/Components/Map/ShapeModifierComponent.cpp
@@ -4,23 +4,3 @@
 
 #include "ShapeComponent.h"
 #include "ShapeHoverComponent.h"
-
-void mk::ShapeModifierComponent::OnNotify(ISubject* subjectPtr, IEvent* event)
-{
-	IObserver::OnNotify(subjectPtr, event);
-
-	HoverEvent* hoverEventPtr{ dynamic_cast<HoverEvent*>(event) };
-	if (!hoverEventPtr)
-		return;
-
-	switch (hoverEventPtr->type)
-	{
-	case HoverEvent::Type::Enter:
-		hoverEventPtr->shapePtr->SetColor({ 0, 255, 0, 255 });
-		break;
-	case HoverEvent::Type::Exit:
-		hoverEventPtr->Log();
-		hoverEventPtr->shapePtr->SetColor({ 255, 0, 0, 255 });
-		break;
-	}
-}
--- CombatVisualiser/Components/Map/ShapeModifierComponent.h
+++ CombatVisualiser/Components/Map/ShapeModifierComponent.h
@@ -4,7 +4,7 @@
 
 namespace mk
 {
-	class ShapeModifierComponent : public IComponent, public IObserver
+	class ShapeModifierComponent : public IComponent
 	{
 	public:
 		ShapeModifierComponent() = default;
@@ -13,8 +13,6 @@ namespace mk
 		ShapeModifierComponent(ShapeModifierComponent&& other) noexcept				= delete;
 		ShapeModifierComponent& operator=(const ShapeModifierComponent& other)		= delete;
 		ShapeModifierComponent& operator=(ShapeModifierComponent&& other) noexcept	= delete;
-
-		void OnNotify(ISubject* subjectPtr, IEvent* event) override;
 
 	private:
 	};
